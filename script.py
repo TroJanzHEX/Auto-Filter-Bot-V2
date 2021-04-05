0e2b9845c139188ff63be79c02a517e0f7698bb5
Jinshid <79472299+Jinshid@users.noreply.github.com>
2021-04-05 15:23:28 +0400

    Update script.py

--- script.py
+++ script.py
@@ -5,64 +5,20 @@ class script(object):
 
 This Bot Is Made For @CinemaFactoryGroup!
 
-
-
-
-
-
 For more click <i>help</i></b>"""
 
 
     HELP_MSG = """<b>Sorry..🥺</b>
 
-<i>
-* No Info
-
-* No Info <b> No Info </b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
 
 <b>© @CinemaFactoryGroup"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+    ABOUT_MSG = """⭕️<b>My Name : Sukumara Kurup </b>
     
 ⭕️<b>Creater :</b> @CinemaFactoryGroup
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
-⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
 
 """
