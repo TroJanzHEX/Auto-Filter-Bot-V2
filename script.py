870de9874f4e37e2615a4ec082aae5d165b3bae9
Jinshid <79472299+Jinshid@users.noreply.github.com>
2021-04-05 16:44:43 +0400

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,15 @@
 class script(object):
 
 
-    START_MSG = """ <b>Hi {}
-
-This Bot Is Made For @CinemaFactoryGroup!
-
-For more click <i>help</i></b>"""
-
+    START_MSG = """ <b>This Bot Is Made For @CinemaFactoryGroup!</b>"""
 
     HELP_MSG = """<b>Sorry..🥺</b>
 
-
 <b>© @CinemaFactoryGroup"""
-
 
     ABOUT_MSG = """⭕️<b>My Name : Sukumara Kurup </b>
     
 ⭕️<b>Creater :</b> @CinemaFactoryGroup
-
-⭕️<b>Language :</b> <code>Python3</code>
 
 
 """
