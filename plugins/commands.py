0eed2240fc2ede5e08e18124d87ebe7661bf6a11
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-07-17 23:25:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,78 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @trojanzhex
+# (c) @KicchaRequest
 
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>Am Just A Kiccha Auto Filter Bot....😉
 
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from script import script
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
+For More Details Click Help Button Below..
+@KicchaRequest
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
 
-@Client.on_message(filters.command(["start"]) & filters.private)
-async def start(client, message):
-    try:
-        await message.reply_text(
-            text=script.START_MSG.format(message.from_user.mention),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("HELP", callback_data="help_data"),
-                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
-                    ]
-                ]
-            ),
-            reply_to_message_id=message.message_id
-        )
-    except:
-        pass
+<i>
+-> Add Me To Any Group And Make Me Admin
+-> Add Me To Your Desired Channel
+</i>
 
-@Client.on_message(filters.command(["help"]) & filters.private)
-async def help(client, message):
-    try:
-        await message.reply_text(
-            text=script.HELP_MSG,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("BACK", callback_data="start_data"),
-                        InlineKeyboardButton("ABOUT", callback_data="about_data"),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
-                    ]
-                ]
-            ),
-            reply_to_message_id=message.message_id
-        )
-    except:
-        pass
+<b>Bot Commands (Works Only In Groups) :</b>
 
-@Client.on_message(filters.command(["about"]) & filters.private)
-async def about(client, message):
-    try:
-        await message.reply_text(
-            text=script.ABOUT_MSG,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("BACK", callback_data="help_data"),
-                        InlineKeyboardButton("START", callback_data="start_data"),
-                    ],
-                    [
-                        InlineKeyboardButton(
-                            "SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")
-                    ]
-                ]
-            ),
-            reply_to_message_id=message.message_id
-        )
-    except:
-        pass
+    -> <code>/kicchaadd chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/kicchaadd @Username</code>
+     
+    -> <code>/kicchadel chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/kicchadel @Username</code>
+     
+    -> <code>/kicchadelall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+@KicchaRequest
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    
+<b>➥ Creator</b> : <b><i><a href="https://t.me/sachin_official_admin">Sachin_S</a></i></b>
+
+<b>➥ Language</b> : <code>Python3</code>
+
+<b>➥ Library</b> : <i><a href="https://t.me/ROCKHDMOVIES2021">Pyrogram Asyncio 1.13.0 </a></i>
+
+<b>➥ Source Code</b> : <i><a href="https://t.me/KicchaRequest">Click Me</a></i>
+"""
