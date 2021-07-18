3f38741f99cb47ae2f41450b259b34f8e9929057
Sachin S <86321236+sachin9742s@users.noreply.github.com>
2021-07-18 16:47:35 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -62,7 +62,7 @@ async def filter(client: Bot, message: Message):
                 [InlineKeyboardButton(text="🎶 Pages 1/1 🎶",callback_data="pages")]
             )
             await message.reply_text(
-                f"<b> Here is the result for {message.text}</b>",
+                f"<b>Here is the result for {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )
             return
@@ -78,7 +78,7 @@ async def filter(client: Bot, message: Message):
         )
 
         await message.reply_text(
-                f"<b> Here is the result for {message.text}</b>",
+                f"<b>Here is the result for {message.text}</b>",
                 reply_markup=InlineKeyboardMarkup(buttons)
             )    
 
