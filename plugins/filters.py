9abefaa616b22e23f15decd0f8360a980716aef7
ANKIT3690 <93529237+ANKIT3690@users.noreply.github.com>
2021-11-20 22:49:57 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -169,7 +169,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR GROUP ⭕️", url="https://t.me/defenderofthemultiverse")]
+                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/defenderofthemultiverse")]
             ])
 
             await query.message.edit_text(
@@ -184,7 +184,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/thewarriorsreal")]
+                [InlineKeyboardButton("⭕️ JOIN OUR GROUP ⭕️", url="https://t.me/thewarriorsreal")]
             ])
 
             await query.message.edit_text(
